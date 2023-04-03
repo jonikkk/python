c3129b0002ad0be5a5f054bb011c615a5805b7b5
jonikkk <eugene.evtyshenko@gmail.com>
2023-04-03 21:44:26 +0200

    Add files via upload

---
+++ Home_work_2.py
@@ -0,0 +1,14 @@
+a = 2
+b = 5
+c = a == b
+print(c)
+String = 'Hello'
+print(str(a-b) + String)
+list_1=[a,b,c,String]
+print(list_1)
+print(set(list_1))
+tup_1 = (1,2,3,4)
+tup_2 = ('A','B','C','D')
+tup_sum = tup_1+tup_2
+print(list(tup_sum))
+
