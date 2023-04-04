585685e0c70ebad636613343f711c8b4ec150f1b
jonikkk <eugene.evtyshenko@gmail.com>
2023-04-04 18:30:13 +0200

    Add files via upload

--- Home_work_2.py
+++ Home_work_2.py
@@ -4,11 +4,13 @@ c = a == b
 print(c)
 String = 'Hello'
 print(str(a-b) + String)
-list_1=[a,b,c,String]
+list_1 = [a, b, c, String]
 print(list_1)
 print(set(list_1))
-tup_1 = (1,2,3,4)
-tup_2 = ('A','B','C','D')
+tup_1 = (1, 2, 3, 4)
+tup_2 = ('A', 'B', 'C', 'D')
 tup_sum = tup_1+tup_2
 print(list(tup_sum))
+new_dic = {'Age': 18, 'Name': 'Eugene', 178: 'Height', True: None, False: ['No'], 30.5: {'volume': True},
+           'Men': 'Men', 3: 30, (1, 2, 3): (4, 5, 6)}
 
